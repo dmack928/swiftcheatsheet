0f1285c51d9d1fffe9e250898bbab160ce83f23d
Danny Mackey <dmack928@gmail.com>
2019-03-11 16:57:41 -0400

    updated arrays

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -31,4 +31,50 @@ calcString = calcString + calResultString
 
 // Arrays
 
-let arrayInt :[Int] = [1,2,3,4,5,6,7,8,9,10]
+var arrayInt :[Int] = [1,2,3,4,5,6,7,8,9,10]
+
+var arrayStr :[String] = ["bob","bill", "mike"]
+
+let name = arrayStr
+
+//access the array
+
+let age = arrayInt.last!
+//accessing the array
+
+let message = "My name is \(name[0])"
+
+let message1 = "I am \(age) years old"
+
+let message2 = "My name is \(name[2])"
+
+// push to an end of an array
+arrayInt.append(12)
+
+arrayStr.append("Taco")
+
+// insert into an array
+arrayInt.insert(1, at: 0)
+
+arrayStr.insert("Now I'm First", at: 0)
+
+//reassigning values in an array
+
+arrayInt[0] = 12
+
+arrayInt
+
+arrayStr[1] = "Amelia"
+
+arrayStr
+
+//removing values from an array
+//removes last inde of the array
+arrayStr.removeLast()
+//notice no tacos :(
+arrayStr
+// to remove at a specific index
+arrayStr.remove(at: 3)
+// bye Mike
+arrayStr
+
